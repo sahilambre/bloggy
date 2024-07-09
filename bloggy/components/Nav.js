e7577b5ff3e461403893270c1d168d55c177f0b4
Sahil Mukesh Ambre <59484827+sahilambre@users.noreply.github.com>
2024-07-09 01:36:44 -0400

    user authentication firebase

---
+++ bloggy/components/Nav.js
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { auth } from "@/utils/firebase";
+import { getAuth } from "firebase/auth";
+import { useAuthState } from "react-firebase-hooks/auth";
+
+export default function Nav() {
+  const [user, loading] = useAuthState(auth);
+  //   console.log(user);
+  return (
+    <nav className="flex justify-between items-center py-10">
+      <Link href="/">
+        <button className="text-lg font-medium">Creative Minds</button>
+      </Link>
+      <ul className="flex items-center gap-10">
+        {!user && (
+          <li className="py-2 px-4 text-sm bg-cyan-500 text-white rounded-lg font-medium">
+            <Link href="/auth/login">Join Now</Link>
+          </li>
+        )}
+        {user && (
+          <div className="flex item-center gap-6">
+            <Link href="/post">
+              <button className="font-medium bg-cyan-500 text-white py-2 px-4 rounded-mg textx-small">
+                Post
+              </button>
+            </Link>
+            <Link href="/dashboard">
+              <img
+                className="w-12 rounded-full cursor-pointer"
+                src={user.photoURL}
+              />
+            </Link>
+          </div>
+        )}
+      </ul>
+    </nav>
+  );
+}
